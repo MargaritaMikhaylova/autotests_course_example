911de758bb56e2201f6dee3e208f8d8e155b4974
MargaritaMikhaylova <margooptimistic@gmail.com>
2023-05-31 02:11:19 +0300

    ДЗ7

---
+++ Homework7/task_2.py
@@ -0,0 +1,99 @@
+# Напишите класс PersonInfo
+# Экземпляр класса создается из следующих атрибутов:
+# 1. Строка - "Имя Фамилия"
+# 2. Число - возраст сотрудника
+# 3. Подразделения от головного до того, где работает сотрудник.
+# Реализуйте методы класса:
+# 1. short_name, который возвращает строку Фамилия И.
+# 2. path_deps, возвращает путь "Головное подразделение --> ... --> Конечное подразделение"
+# 3. new_salary, Директор решил проиндексировать зарплаты, и новая зарпалата теперь вычисляет по формуле:
+# 1337*Возраст*суммарное кол-во вхождений трех наиболее часто встречающихся букв из списка подразделений
+# (регистр имеет значение "А" и "а" - разные буквы)
+# Например (Ввод --> Вывод) :
+# PersonInfo('Александр Шленский',
+#            32,
+#            'Разработка', 'УК', 'Автотесты').short_name() --> 'Шленский А.'
+# PersonInfo('Александр Шленский',
+#            32,
+#            'Разработка', 'УК', 'Автотесты').path_deps() -->
+#            'Разработка --> УК --> Автотесты'
+# PersonInfo('Александр Шленский', 32, 'Разработка', 'УК', 'Автотесты').new_salary() --> 385056 т.к.
+# т.к. буква "т" встречается 4 раза, "а" 3 раза, 'о' 2 раза, остальные по одной. Сумма трёх самых частых букв 4+3+2 = 9.
+# 1337*32*9 = 385056
+
+class PersonInfo:
+
+    def __init__(self, name, age, *department):
+        self.name = name
+        self.age = age
+        self.department = department
+
+    def short_name(self):
+        '''
+        Метод возвращает строку Фамилия И.
+        :return: Фамилия И.
+        '''
+        short_name = self.name.split()
+        short_name = short_name[1] + ' ' + short_name[0][0] + '.'
+        return short_name
+
+    def path_deps(self):
+        '''
+        Метод возвращает путь подразделения сотрудника от головного
+        :return: "Головное подразделение --> ... --> Конечное подразделение"
+        '''
+        path_deps = ' --> '.join(self.department)
+        return path_deps
+
+    def new_salary(self):
+        '''
+        Метод возвращает размер проиндексированной зарплаты
+        :return: Сумма новой зарплаты
+        '''
+        list_keys = list(''.join(self.department))
+        letters_dict = {item: list_keys.count(item) for item in list_keys}
+        max_letters = sorted(letters_dict.items(), key=lambda item: item[1])
+        koef = max_letters[-1][1] + max_letters[-2][1] + max_letters[-3][1]
+        new_salary = 1337 * self.age * koef
+        return new_salary
+
+# Здесь пишем код
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+first_person = PersonInfo('Александр Шленский', 32, 'Разработка', 'УК', 'Автотесты')
+fourth_person = PersonInfo('Иван Иванов', 26, 'Разработка')
+second_person = PersonInfo('Пётр Валерьев', 47, 'Разработка', 'УК')
+third_person = PersonInfo('Макар Артуров', 51, 'Разработка', 'УК', 'Нефункциональное тестирование', 'Автотестирование')
+
+data = [first_person.short_name,
+        second_person.short_name,
+        third_person.short_name,
+        fourth_person.short_name,
+
+        first_person.path_deps,
+        second_person.path_deps,
+        third_person.path_deps,
+        fourth_person.path_deps,
+
+        first_person.new_salary,
+        second_person.new_salary,
+        third_person.new_salary,
+        fourth_person.new_salary
+        ]
+
+
+test_data = ['Шленский А.', 'Валерьев П.', 'Артуров М.', 'Иванов И.',
+
+             'Разработка --> УК --> Автотесты',
+             'Разработка --> УК',
+             'Разработка --> УК --> Нефункциональное тестирование --> Автотестирование',
+             'Разработка',
+             385056, 314195, 1227366, 173810]
+
+for i, d in enumerate(data):
+    assert_error = f'Не прошла проверка для метода {d.__qualname__} экземпляра с атрибутами {d.__self__.__dict__}'
+    assert d() == test_data[i], assert_error
+    print(f'Набор для метода {d.__qualname__} экземпляра класса с атрибутами {d.__self__.__dict__} прошёл проверку')
+print('Всё ок')
